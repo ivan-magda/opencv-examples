f87374212255f3e1c7b51c78e069b665f066f665
Ivan Magda <imagda15@gmail.com>
2017-03-02 17:03:17 +0900

    chore: Add image segmentation example project
    Apply to video stream a Canny edge detector or a trivial background removal using the two basic morphological operations: dilatation and erosion.

---
+++ ImageSegmentation/src/com/ivanmagda/cv/Main.java
@@ -0,0 +1,30 @@
+package com.ivanmagda.cv;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+public class Main extends Application {
+
+    private static final int SCENE_WIDTH = 640;
+    private static final int SCENE_HEIGHT = 480;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        BorderPane root = loader.load();
+        primaryStage.setTitle("Image Segmentation");
+        primaryStage.setScene(new Scene(root, SCENE_WIDTH, SCENE_HEIGHT));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        // Load the native OpenCV library.
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        launch(args);
+    }
+}
