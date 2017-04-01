347310b917403ed0a6be4680903a76a2b5ab13b9
Ivan Magda <imagda15@gmail.com>
2017-04-01 17:13:18 +0900

    feat: [iOS-AR] Initial commit

---
+++ iOS-AR/iOS-AR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-AR
+//
+//  Created by Ivan Magda on 01/04/2017.
+//  Copyright © 2017 Ivan Magda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
